75adc27553831f72e19642f6aef91695989e997a
pie-tro <pietromaia28@gmail.com>
2025-11-05 19:58:59 -0300

    create metodos e atributos medicacao.java

--- prjPOOPietroArturAna/src/fatec/poo/model/Medicacao.java
+++ prjPOOPietroArturAna/src/fatec/poo/model/Medicacao.java
@@ -9,5 +9,31 @@ package fatec.poo.model;
  * @author pietro
  */
 public class Medicacao {
+    private String nome;
+    private String dosagem;
+    private int qtdeDias;
+
+    
+    public void setDosagem(String dosagem) {
+        this.dosagem = dosagem;
+    }
+
+    public void setQtdeDias(int qtdeDias) {
+        this.qtdeDias = qtdeDias;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDosagem() {
+        return dosagem;
+    }
+
+    public int getQtdeDias() {
+        return qtdeDias;
+    }
+    
+    
     
 }
