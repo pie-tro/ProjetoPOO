4c4dff2043ad4da7658e16b63a890a683792cfd8
pie-tro <pietromaia28@gmail.com>
2025-11-05 20:07:55 -0300

    create metodos exame.java
    create metodos construtor, getters and setters e atributos

--- prjPOOPietroArturAna/src/fatec/poo/model/Exame.java
+++ prjPOOPietroArturAna/src/fatec/poo/model/Exame.java
@@ -5,5 +5,48 @@ package fatec.poo.model;
  * @author pietro
  */
 public class Exame {
+    private int codigo;
+    private String descricao;
+    private String data;
+    private String horario;
+    private double valor;
+
+    public Exame(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+    
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    
     
 }
