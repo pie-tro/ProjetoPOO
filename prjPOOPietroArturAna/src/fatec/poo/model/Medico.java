d32d978d7fff82d57f4fecad6af71c87daadb3eb
pie-tro <pietromaia28@gmail.com>
2025-11-05 19:54:11 -0300

    feat: atributos e metodos medico.java
    atributos e metodos construtor e getter da classe medico

--- prjPOOPietroArturAna/src/fatec/poo/model/Medico.java
+++ prjPOOPietroArturAna/src/fatec/poo/model/Medico.java
@@ -5,5 +5,19 @@ package fatec.poo.model;
  * @author pietro
  */
 public class Medico {
-    
+    private String crm;
+    private String especialidade;
+
+    public Medico(String crm, String especialidade) {
+        this.crm = crm;
+        this.especialidade = especialidade;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
 }
