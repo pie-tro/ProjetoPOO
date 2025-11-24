6f335154daea155dff9040331ed9bcdce6d47914
Artur Henrique Cruz da Silva <arturcruz2505@gmail.com>
2025-11-24 19:50:28 -0300

    Implementação Validar CPF
    implementação método ValidarCPF na classe pessoa e inclusão de bibliotecas

--- prjPOOPietroArturAna/src/fatec/poo/model/Pessoa.java
+++ prjPOOPietroArturAna/src/fatec/poo/model/Pessoa.java
@@ -38,6 +38,39 @@ public class Pessoa {
     public String getTelefone() {
         return telefone;
     }
+    public static boolean validarCPF(String cpf)
+    {
+     if(cpf.length()!=11)  {
+         return false;
+     } 
+     if (cpf.chars().distinct().count() == 1) {
+    return false;
+} 
+     int soma1 = 0;
+    for (int i = 0; i < 9; i++) {
+        int numero = cpf.charAt(i) - '0'; 
+        soma1 += (numero * (i + 1));
+    }
     
+    int resto1 = soma1 % 11;
+    int digito1 = (resto1 == 10) ? 0 : resto1;
+     
+    int soma2 = 0;
+    int peso2 = 11;
+    
+    for (int i = 0; i < 9; i++) {
+        int numero = cpf.charAt(i) - '0';
+        soma2 += (numero * peso2);
+        peso2--;
+    }
+    
+    soma2 += (digito1 * 2);
+
+    int resto2 = (soma2 * 10) % 11;
+    int digito2 = (resto2 == 10) ? 0 : resto2;
+    
+    return (digito1 == (cpf.charAt(9) - '0')) && (digito2 == (cpf.charAt(10) - '0'));
+        
+    }
     
 }
