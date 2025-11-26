46c5c0f3e239e214a93a4cdfc1eddb4a9990e834
pie.tro <pietromaia28@gmail.com>
2025-11-26 16:36:02 -0300

    create guiexame

--- prjPOOPietroArturAna/src/fatec/poo/model/Exame.java
+++ prjPOOPietroArturAna/src/fatec/poo/model/Exame.java
@@ -29,6 +29,10 @@ public class Exame {
         this.valor = valor;
     }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
     public int getCodigo() {
         return codigo;
     }
